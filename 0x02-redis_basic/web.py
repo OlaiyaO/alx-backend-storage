0c1718321d86fc2d2879c70bd31f7ed554e935bc
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-08-14 20:03:36 +0100

    Worked throught

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,27 +8,30 @@ from functools import wraps
 
 store = redis.Redis()
 
-
 def count_url_access(method):
     """Decorator to count how many times a URL is accessed."""
-    url = "http://slowwly.robertomurray.co.uk"
-
     @wraps(method)
-    def wrapper(url):
-        cached_key = "cached:" + url
+    def wrapper(*args, **kwargs):
+        # The URL is passed in the arguments
+        url = args[0]
+        cached_key = f"cached:{url}"
         cached_data = store.get(cached_key)
+
+        # Return cached data if it exists
         if cached_data:
             return cached_data.decode("utf-8")
 
-        count_key = "count:" + url
-        html = method(url)
+        # Get the HTML from the original function if not cached
+        count_key = f"count:{url}"
+        html = method(*args, **kwargs)
 
+        # Increment the count for the URL and store the result in cache
         store.incr(count_key)
         store.set(cached_key, html)
-        store.expire(cached_key, 10)
+        store.expire(cached_key, 10)  # Cache expires in 10 seconds
+
         return html
     return wrapper
-
 
 @count_url_access
 def get_page(url: str) -> str:
